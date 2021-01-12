3d53609221fed302c40c8d74efca5e0bd35b4cb2
brunoconde07 <brunoconde.ufpa@gmail.com>
2021-01-12 05:12:03 -0300

    (index.html and index.js, at hcode/project_6) -> swich/case

---
+++ hcode/project_6/index.js
@@ -0,0 +1,15 @@
+var color = "blue";
+
+switch (color) {
+	case "green":
+		console.log("Go!");
+		break;
+	case "red":
+		console.log("Stop!");
+		break;
+	case "yellow":
+		console.log("Atention!");
+		break;
+	default:
+		console.log("invalid color");
+}
