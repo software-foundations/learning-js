8db34368f85c29ca627bddfded134873756509e0
brunoconde07 <brunoconde.ufpa@gmail.com>
2021-01-14 00:16:20 -0300

    (project_02/, at hcode/basic_04) -> unix time stamp well formated like this: Thu Jan 14 2021 00:13:46 GMT-0300 (Brasilia Standard Time)

---
+++ hcode/basic_04/project_02/index.js
@@ -0,0 +1,7 @@
+// unix time stamp: second since 1970, jan, 01
+// this is more well formated
+let current_unix_time_stamp = new Date();
+
+
+console.log(current_unix_time_stamp);
+// Thu Jan 14 2021 00:13:46 GMT-0300 (Brasilia Standard Time)
