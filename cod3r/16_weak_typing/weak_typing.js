45d99c2b672b44d024adb0f399461170c29e3a8b
brunoconde07 <brunoconde.ufpa@gmail.com>
2021-03-17 17:08:33 -0300

    (/cod3r/16_weak_typing/) -> js is dynamically and type, so it is weakly typed

---
+++ cod3r/16_weak_typing/weak_typing.js
@@ -0,0 +1,19 @@
+let some = 'cool'
+
+console.log(some)
+
+console.log(typeof some)
+
+some = 3.15
+
+console.log(some);
+
+console.log(typeof some)
+
+// avoid generic names and acronyms
+
+let value = ''
+
+let number = 123
+
+let pqp = false // produto químico perigoso
