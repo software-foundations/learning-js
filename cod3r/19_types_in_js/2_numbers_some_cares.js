9d9c498f5bcda358c2d45e5cc7ad4d7e542efa14
brunoconde07 <brunoconde.ufpa@gmail.com>
2021-05-14 11:01:09 -0300

    (/cod3r/19_types_in_js/2_numbers_some_cares.js) -> 20: Number: some cares

---
+++ cod3r/19_types_in_js/2_numbers_some_cares.js
@@ -0,0 +1,6 @@
+console.log(7 / 0) // -> Infinity
+console.log("10" / 2) // -> 5
+console.log("show" * 2) // -> NaN
+console.log(0.1 + 0.7) // -> 0.7999999999999999 -> IEEE Standard for Binary FloatPoint Arithmetic
+// console.log(10.toString) // -> SyntaxError
+console.log((10.345).toFixed(2)) // -> 10.35
