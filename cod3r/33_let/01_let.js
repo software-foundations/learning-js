78d4fa7436449ed49add89356da67d03a408dbd1
brunoconde07 <brunoconde.ufpa@gmail.com>
2021-05-14 22:24:25 -0300

    (/cod3r/33_let/01_let.js) -> 33: Variable Declaration with Let

---
+++ cod3r/33_let/01_let.js
@@ -0,0 +1,11 @@
+let number = 1
+
+{
+	let number = 2 // Try comment this line
+	console.log('inside = ', number)
+}
+
+console.log('outside = ', number)
+
+// variables declared with var have global and function scope
+// variables declared with let have global, function and block scope
