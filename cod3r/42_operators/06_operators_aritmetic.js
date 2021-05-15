c6bc23b606a3b4a52f547ebe49e85ab0578c0d6a
brunoconde07 <brunoconde.ufpa@gmail.com>
2021-05-15 18:55:38 -0300

    (/cod3r/42_operators/06_operators_aritmetic.js) -> 47: Operators: Aritmetic

---
+++ cod3r/42_operators/06_operators_aritmetic.js
@@ -0,0 +1,9 @@
+const [a, b, c, d] = [3, 5, 1, 15]
+
+const sum = a + b + c + d
+const subtraction = b - d
+const multiplication = a * b
+const division = d / a
+const mod = a % 2
+
+console.log(sum, subtraction, multiplication, division, mod)
